91e7840995ac4dfc809c714bca1925d5340ce1c1
Omer Gez <76686705+OmerGez9001@users.noreply.github.com>
2022-01-04 14:40:57 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,17 +1,17 @@
 import requests #testing the requests to the server  
   
 base = "http://localhost:5000/"
-response = requests.put(base + "DataBase/" + str(2), {"Name": "Omer", "Phone": "0506498555"})
+response = requests.put(base + "Clients/" + str(2), {"Name": "Omer", "Phone": "0506498555"})
 print(response)
 print(response.json())
-response = requests.get(base + "DataBase/" + str(2))
+response = requests.get(base + "Clients/" + str(2))
 print(response)
 print(response.json())
-response = requests.get(base + "DataBase/" + str(3))
+response = requests.get(base + "Clients/" + str(3))
 print(response.content)
-response = requests.put(base + "DataBase/" + str(3), {"Name": "Omer", "Phone": "0506498555"})
-response = requests.put(base + "DataBase/" + str(4), {"Name": "Omer", "Phone": "0506498555"})
-response = requests.get(base + "DataBase/" + str(2))
+response = requests.put(base + "Clients/" + str(3), {"Name": "Omer", "Phone": "0506498555"})
+response = requests.put(base + "Clients/" + str(4), {"Name": "Omer", "Phone": "0506498555"})
+response = requests.get(base + "Clients/" + str(2))
 print(response)
 print(response.json())
   
